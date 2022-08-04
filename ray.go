57e8d602d6237489bcb90895880814320d34a1ac
Adam Patterson <adam@adamrt.com>
2022-08-03 21:10:51 -0700

    Draw sphere with rays

--- ray.go
+++ ray.go
@@ -14,8 +14,21 @@ func (r Ray) At(t float64) Vec3 {
 
 func (r Ray) Color() color.RGBA {
 	unitDirection := r.direction.Unit()
+	if r.hitSphere(Vec3{0, 0, -1}, 0.5) {
+		return Vec3{1, 0, 0}.RGBA()
+	}
 	t := 0.5 * (unitDirection.y + 1.0)
 	a := Vec3{1.0, 1.0, 1.0}.ScalarMul(1.0 - t)
 	b := Vec3{0.5, 0.7, 1.0}.ScalarMul(t)
 	return a.Add(b).RGBA()
+
+}
+
+func (r Ray) hitSphere(center Vec3, radius float64) bool {
+	oc := r.origin.Sub(center)
+	a := r.direction.Dot(r.direction)
+	b := 2.0 * oc.Dot(r.direction)
+	c := oc.Dot(oc) - radius*radius
+	discriminant := b*b - 4*a*c
+	return (discriminant > 0)
 }
